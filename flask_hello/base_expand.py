d4d91b2fc514f999c18ade8c14d9ff09bd597023
LZD-20230508PLU\Administrator <2446265827@qq.com>
2025-02-13 22:10:38 +0800

    补充flask拓展demo

--- flask_hello/base_expand.py
+++ flask_hello/base_expand.py
@@ -4,9 +4,14 @@
 from flask import Flask
 from flask import abort, redirect, url_for
 from flask import render_template
-from flask import make_response
+from flask import make_response, request, session
 
 base_expand_obj = Flask(__name__)
+base_expand_obj.secret_key = "\x14\x87\xb6-~\x1e\xd8;\xb7\xf7\x84\xb1\x00E\x99-"
+"""
+执行 python -c "import os; print(os.urandom(16))"
+然后cv即可
+"""
 
 
 @base_expand_obj.route("/")
@@ -26,6 +31,30 @@ def handle_401(error):  # 401的统一错误 定制细节]
     res_obj = make_response(render_template("401_detail.html"), 401)
     res_obj.headers["readme"] = "-_-"
     return res_obj
+
+
+@base_expand_obj.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session["user_nm"] = request.form["username"]
+        print(session["user_nm"])
+        return redirect(url_for("login_and_out_logic"))
+
+    return render_template("login.html")
+
+
+@base_expand_obj.route("/logout")
+def logout():
+    session.pop("user_nm", None)
+    return redirect(url_for("login_and_out_logic"))
+
+
+@base_expand_obj.route("/log_logic")
+def login_and_out_logic():
+    if "user_nm" in session:
+        return f"welcome in {session['user_nm']}"
+
+    return "you are not logged in"
 
 
 if __name__ == "__main__":
