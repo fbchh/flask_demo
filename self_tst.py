242971718452d16c4a976853d4a50529562cd3ba
LZD-20230508PLU\Administrator <2446265827@qq.com>
2025-03-10 23:42:46 +0800

    添加官方demo-blog

---
+++ self_tst.py
@@ -0,0 +1,24 @@
+"""
+xxx
+"""
+
+
+def main():
+    # a = {
+    #     "page": 1,
+    #     "theme": "hh"
+    # }
+    # tst_1 = a.get("page", 12)
+    # print(tst_1)
+
+    a = []
+    b = [1, 2, 3]
+    a.extend(b)
+    print(a)
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    ...
